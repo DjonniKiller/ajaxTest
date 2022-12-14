64b3f076f91d110b9596daa367cdcd817f48fa4f
Рябко Евгений <jeniaryanko@mail.ru>
2022-12-14 21:32:37 +0300

    Version_0.2

--- test.js
+++ test.js
@@ -1,43 +1,42 @@
 $(function () {
-    $.ajax({
-        url: 'https://jsonplaceholder.typicode.com/users',
-        success: (data) => {
-            data.forEach(el => {
-                $('#users').append(`<option value=${el.id}>${el['name']}</option>`);
-            });
-        }
-    });
+	$.ajax({
+		url: 'https://jsonplaceholder.typicode.com/users',
+		success: (data) => {
+			data.forEach(el => {
+				$('#users').append(`<option class='select_option' value=${el.id}>${el.name}</option>`);
+			});
+		}
+	});
 
-    $('#users').change((event) => {
-        $('#todos').html('');
-        $('#result').html('');
+	$('#users').change((event) => {
+		$('#todos').html('');
+		$('#result').html('');
 
-        console.log(event.target.value);
-        $.ajax({
-            url: 'https://jsonplaceholder.typicode.com/todos',
-            success: (data) => {
-                data.forEach(el => {
-                    if (parseInt(el['userId']) === parseInt(userId.slice(0,1)))
-                        $('#todos').append(`<option>${el['title']}</option>`);
-                });
-            }
-        });
-    });
+		console.log(event.target.value);
+		$.ajax({
+			url: 'https://jsonplaceholder.typicode.com/todos',
+			success: (data) => {
+				data.forEach(el => {
+					if (parseInt(el['userId']) === parseInt(event.target.value))
+						$('#todos').append(`<option class='select_option' value=${el.id}>${el.title}</option>`);
+				});
+			}
+		});
+	});
 
-    $('#todos').change(() => {
-        let todoId = $("#todos :selected").text();
-        char = todoId.slice(0, 1);
+	$('#todos').change((event) => {
+		$('#result').html('');
 
-        $.ajax({
-            url: 'https://jsonplaceholder.typicode.com/todos',
-            success: (data) => {
-                obj = data.find(obj => {
-                    parseInt(obj['id']) === parseInt(todoId.slice(0, 1));
-                });
+		$.ajax({
+			url: 'https://jsonplaceholder.typicode.com/todos',
+			success: (data) => {
+				obj = data.find(el => {
+					return parseInt(el.id) === parseInt(event.target.value);
+				});
 
-                for (key in obj)
-                    $('#result').append(`<p>${key}: ${obj[key]}</p>`);
-            }
-        });
-    });
+				for (key in obj)
+					$('#result').append(`<li class='list_row'>${key}: ${obj[key]}</li>`);
+			}
+		});
+	});
 });
